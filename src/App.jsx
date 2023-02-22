782077c68840a52040e073d9e424af233284df56
iAGuss <gustavodario2001@gmail.com>
2023-02-22 15:03:03 -0300

    instalacion-tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 // import { useState } from 'react'
 
-
-
 function App() {
   return (
     <div className="App">
-      <h1>holaq</h1>
+      <h1 className=" text-center text-xl md:text-right xl:text-left">holaq</h1>
     </div>
   );
 }
