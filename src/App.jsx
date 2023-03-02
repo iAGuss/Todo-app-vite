b4056c0482fa62fe789661db2b7949bcea657cc3
iAGuss <gustavodario2001@gmail.com>
2023-03-02 19:44:50 -0300

    images--design

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1 className=" text-center text-xl md:text-right xl:text-left">holaq</h1>
+    <div className="h-48 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-cover">
+      <h1 className="display: inline-flex  text-left uppercase">Todo</h1>
+      <button>🌙</button>
     </div>
   );
 }
