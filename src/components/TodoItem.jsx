8c24157fdd1cda0f830f6b3cea7b4253bc26ab79
iAGuss <gustavodario2001@gmail.com>
2023-03-07 23:55:13 -0300

    check -icon

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import CrossIcon from "./icons/CrossIcon";
-
+import ChekIcon from "./icons/ChekIcon";
 function TodoItem({ todo }) {
   return (
     <article className="flex gap-4 border-b-2 border-b-gray-400 px-4 py-4">
-      <button className="inline-block h-5 w-5 rounded-full border-2 "></button>
+      {/* <button className="inline-block h-5 w-5 rounded-full border-2 ">
+        <ChekIcon/>10
+      </button> */}
+      <button className="  flex h-5 w-5 items-center justify-center rounded-full border-2 bg-gradient-to-r from-blue-700 to-blue-400">
+        <ChekIcon />
+      </button>
       <p className="grow text-gray-600">{todo.title}</p>
       <button>
         <CrossIcon />
